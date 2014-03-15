6aa7744e71acf5994e27bad6e442265185732b69
Christian Haas <christian.haas@sevensuns.at>
2014-03-15 21:09:38 +0100

    dropped executor in favor of a mutex - decreases goroutine count by 2 per request

--- travel/search/PathSearchAgent.go
+++ travel/search/PathSearchAgent.go
@@ -1,6 +1,8 @@
 package search
 
 import (
+	"sync"
+
 	"github.com/dertseha/everoute/travel"
 	"github.com/dertseha/everoute/util"
 )
@@ -40,8 +42,8 @@ func (result *basicPathSearchAgentResult) Path(destinationKey string, rand util.
 }
 
 type pathSearchAgent struct {
-	state    pathSearchAgentState
-	executor util.Executor
+	state     pathSearchAgentState
+	stateLock sync.Locker
 }
 
 type pathSearchAgentActiveState struct {
@@ -92,7 +94,7 @@ func newPathSearchAgent(startPath travel.Path, capability travel.TravelCapabilit
 
 		results:       make(map[string]travel.Path),
 		resultsAsList: make([]travel.Path, 0)}
-	agent := &pathSearchAgent{state: state, executor: util.SingleThreadExecutor(0)}
+	agent := &pathSearchAgent{state: state, stateLock: &sync.Mutex{}}
 
 	FindPath(startPath, capability, rule, criterion, state, func() { agent.searchDone(state) })
 
@@ -100,31 +102,33 @@ func newPathSearchAgent(startPath travel.Path, capability travel.TravelCapabilit
 }
 
 func (agent *pathSearchAgent) Request(listener pathSearchAgentListener) {
-	go agent.executor.Execute(func() {
-		agent.state.request(listener)
-	})
+	defer agent.stateLock.Unlock()
+	agent.stateLock.Lock()
+
+	agent.state.request(listener)
 }
 
 func (agent *pathSearchAgent) searchDone(state *pathSearchAgentActiveState) {
-	agent.executor.Execute(func() {
-		if len(state.resultsAsList) > 0 {
-			result := &basicPathSearchAgentResult{
-				bestPath:    nil,
-				paths:       state.results,
-				pathsAsList: state.resultsAsList}
+	defer agent.stateLock.Unlock()
+	agent.stateLock.Lock()
 
-			for _, path := range state.resultsAsList {
-				if (result.bestPath == nil) || (state.rule.Compare(path.CostSum(), result.bestPath.CostSum()) < 0) {
-					result.bestPath = path
-				}
+	if len(state.resultsAsList) > 0 {
+		result := &basicPathSearchAgentResult{
+			bestPath:    nil,
+			paths:       state.results,
+			pathsAsList: state.resultsAsList}
+
+		for _, path := range state.resultsAsList {
+			if (result.bestPath == nil) || (state.rule.Compare(path.CostSum(), result.bestPath.CostSum()) < 0) {
+				result.bestPath = path
 			}
+		}
 
-			agent.state = &pathSearchAgentCompletedState{result: result}
-		} else {
-			agent.state = &pathSearchAgentFailedState{}
-		}
-		for _, listener := range state.requests {
-			agent.state.request(listener)
-		}
-	})
+		agent.state = &pathSearchAgentCompletedState{result: result}
+	} else {
+		agent.state = &pathSearchAgentFailedState{}
+	}
+	for _, listener := range state.requests {
+		agent.state.request(listener)
+	}
 }
